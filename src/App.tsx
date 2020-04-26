3560a48c340d5b578d89954ebd0601cde052d042
EnixCoda <enixcoda@gmail.com>
2020-04-27 01:26:18 +0800

    remove label counter

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,11 @@ import { QuoteOfTheDay } from "./components/QuoteOfTheDay";
 import { RandomDogApp } from "./components/RandomDogApp";
 import * as fromCrank from "./recrank";
 import { AsyncComponent } from "./recrank/async";
-import { AsyncGeneratorComponent } from "./recrank/asyncGenerator";
 import { StatefulComponent } from "./recrank/stateful";
 import "./styles.css";
 import { sleep } from "./utils";
 
 export default function App() {
-  const [message, setMessage] = React.useState("");
   return (
     <div>
       <h2>StatefulComponent</h2>
@@ -23,13 +21,6 @@ export default function App() {
       <QuoteOfTheDay />
       <hr />
       <h2>AsyncGeneratorComponent</h2>
-      <h4>Labeled Counter</h4>
-      <input
-        placeholder="message"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      />
-      <AsyncGeneratorComponentLabeledCounter message={message} />
       <h4>RandomDogApp</h4>
       <RandomDogApp />
       <hr />
@@ -63,23 +54,3 @@ const Timeout: AsyncComponent<{}> = async function Timeout(props) {
 };
 
 const AsyncComponentTimeout = fromCrank.async(Timeout);
-
-const AsyncLabeledCounter: AsyncGeneratorComponent<{
-  message?: string;
-}> = async function* AsyncLabeledCounter({ message }) {
-  let count = 0;
-  for await ({ message } of this) {
-    yield <div>Loading...</div>;
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    count++;
-    yield (
-      <div>
-        {message} {count}
-      </div>
-    );
-  }
-};
-
-const AsyncGeneratorComponentLabeledCounter = fromCrank.asyncGenerator(
-  AsyncLabeledCounter
-);
